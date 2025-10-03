171dd4b1d8cfea1577687f84640ba27f7f9634f2
Maxamed459 <maxamedmahdi459@gmail.com>
2025-10-03 11:00:32 +0300

    add static app

---
+++ lesson3-StaticAssets/StaticAssets/static/static/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  alert("your js is running successfully!");
+});
+
+const hello = () => {
+  alert("Hello, welcome to django Static and Assets");
+};
