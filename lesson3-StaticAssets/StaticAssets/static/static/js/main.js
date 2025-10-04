1872f8e8c590631eb759b3332762476e9b9fc691
Maxamed459 <maxamedmahdi459@gmail.com>
2025-10-04 22:17:11 +0300

    updated the js/main.js file

--- lesson3-StaticAssets/StaticAssets/static/static/js/main.js
+++ lesson3-StaticAssets/StaticAssets/static/static/js/main.js
@@ -5,19 +5,3 @@ document.addEventListener("DOMContentLoaded", () => {
 const hello = () => {
   alert("Hello, welcome to django Static and Assets");
 };
-
-
-// two sum
-const twoSum = (arr, target) => {
-  for (let i = 0; i < arr.length; i++){
-    for (j = i; j < arr.length; j++) {
-      if ([i] + [j] === target) {
-        console.log(i, j);
-        return i, j
-      }
-      // console.log("please enter valid numbers")
-    }
-  }
-}
-
-console.log(twoSum([1, 2, 3, 4, 5], 6));
