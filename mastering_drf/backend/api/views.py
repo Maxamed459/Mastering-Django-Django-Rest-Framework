c717d0569b6d4cbec33cc9372a4fca806b294e7a
Maxamed459 <maxamedmahdi459@gmail.com>
2025-10-11 12:02:42 +0300

    modified the views of api app

--- mastering_drf/backend/api/views.py
+++ mastering_drf/backend/api/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
-# Create your views here.
+
+
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is Django API response"})
+    body = request.body # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['headers'] = dict(request.headers) # request.META
+    print(request.headers)
+    print(request.GET) # url quey params
+    print(request.POST)
+    data['params'] = dict(request.GET)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
  
