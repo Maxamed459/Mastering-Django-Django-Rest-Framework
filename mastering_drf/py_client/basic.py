d337fdd206bccce54a39df94960e4958f6c2e1e0
Maxamed459 <maxamedmahdi459@gmail.com>
2025-10-11 12:03:19 +0300

    modified the basic api test

--- mastering_drf/py_client/basic.py
+++ mastering_drf/py_client/basic.py
@@ -1,9 +1,23 @@
 import requests
 
-endpoint = "https://jsonplaceholder.typicode.com/users"
-endpoint = "http://localhost:8000/api/"
+# endpoint = "https://jsonplaceholder.typicode.com/users"
+endpoint = "http://localhost:8000/api/books/"
 
-response_data = requests.get(endpoint)
+data = {
+        "id": 3,
+        "title": "Django REST Framework in Action",
+        "author": "Maxamed Mahdi",
+    }
+    # {
+    #     "title": "Rich Dad and Boor Dad",
+    #     "author": "Rich Dad",
+    #     "ISBN": "12345",
+    #     "price": 12.99
+    # },
+    
 
-print(response_data.json()['message'])
+response_data = requests.post(endpoint, json=data)
+
+# print(response_data.status_code)
+print(response_data.json())
 # print(response_data.status_code)
