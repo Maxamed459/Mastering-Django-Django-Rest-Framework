a87784dff95f77ce34993e95c1072735bf385d6e
Maxamed459 <maxamedmahdi459@gmail.com>
2025-10-16 11:05:28 +0300

    add custom permissions

--- project1/api/products/views.py
+++ project1/api/products/views.py
@@ -1,20 +1,24 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from .models import Product
 from .serializers import ProductSerializer
+from .permissions import IsOwnerOrReadOnlyOnly
 
 class CreateListProduct(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsOwnerOrReadOnlyOnly]
 
     def perform_create(self, serializer):
+        owner = self.request.user
         title = serializer.validated_data.get('title')
         description = serializer.validated_data.get('description')
         if description is None:
             description = title
-            serializer.save(description=description)
+            serializer.save(description=description, owner=owner)
         serializer.save()
 
 class RetrieveUpdateDestroyProduct(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsOwnerOrReadOnlyOnly]
     lookup_field = 'pk'
