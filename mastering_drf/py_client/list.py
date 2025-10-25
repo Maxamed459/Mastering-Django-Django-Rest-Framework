23d9f4a9794df1d8dbceb9b422bb376796ed8bf0
Maxamed459 <maxamedmahdi459@gmail.com>
2025-10-25 15:47:03 +0300

    add pagination form request

--- mastering_drf/py_client/list.py
+++ mastering_drf/py_client/list.py
@@ -5,19 +5,29 @@ from getpass import getpass
 auth_endpoint = "http://localhost:8000/api/auth"
 
 username = input("Enter your username\n")
-password = getpass("Enter your username\n")
+password = getpass("Enter your password\n")
 
 auth_response_data = requests.post(auth_endpoint, json={'username': username, 'password': password})
 # print(auth_response_data.json())
 
 if auth_response_data.status_code == 200:
     token = auth_response_data.json()['token']
+    # print(token)
     headers = {
         "Authorization": f"Bearer {token}"
     }
     endpoint = "http://localhost:8000/api/books"
     response_data = requests.get(endpoint, headers=headers)
-    print(response_data.json())
+    # print(response_data.json())
+    data = response_data.json()
+    print(data)
+    print("here is the other requests")
+    next_url = data['next']
+    if next_url is not None:
+        response_data = requests.get(next_url, headers=headers)
+        print(data)
+
+
 # print(auth_response_data.status_code)
 
 # print(response_data.status_code)
