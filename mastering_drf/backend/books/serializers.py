8de1f082b250328b3e54e49d9461e51edee4c8bc
Maxamed459 <maxamedmahdi459@gmail.com>
2025-10-25 15:49:02 +0300

    add changes

--- mastering_drf/backend/books/serializers.py
+++ mastering_drf/backend/books/serializers.py
@@ -14,6 +14,7 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = [
+            # 'user',
             'url',
             'id',
             'title',
