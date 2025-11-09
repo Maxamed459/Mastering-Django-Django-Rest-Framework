2b99051cc7070b864e25bebe4306b0b7105e02a5
Maxamed459 <maxamedmahdi459@gmail.com>
2025-11-09 22:07:36 +0300

    added jwt configuration to the setting

--- mastering_drf/backend/drf1/settings.py
+++ mastering_drf/backend/drf1/settings.py
@@ -146,7 +146,7 @@ AUTH_USER_MODEL = 'product.User'
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        # "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
