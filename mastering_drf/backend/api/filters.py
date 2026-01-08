0cd99956d43503f35cbfaaf249f971793a6fd01d
Maxamed459 <maxamedmahdi459@gmail.com>
2026-01-08 19:36:27 +0300

    added inStockFilterBackend

--- mastering_drf/backend/api/filters.py
+++ mastering_drf/backend/api/filters.py
@@ -1,5 +1,11 @@
 import django_filters
 from product.models import Product
+from rest_framework import filters
+
+
+class inStockFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(stock__gt=0)
 
 class ProductFilter(django_filters.FilterSet):
     class Meta:
