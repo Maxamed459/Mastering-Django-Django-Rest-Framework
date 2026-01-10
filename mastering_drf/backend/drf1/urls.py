013da5161260eb2d8d808bbb9abd85a9478d7963
Maxamed459 <maxamedmahdi459@gmail.com>
2026-01-10 20:24:58 +0300

    updated the product endpoints

--- mastering_drf/backend/drf1/urls.py
+++ mastering_drf/backend/drf1/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('api/', include('api.urls')), # localhost:8000/api/
     path('api/books/', include('books.urls')),
     path('api/v2/', include('drf1.routers')),
-    path('api/product/', include('product.urls')),
+    path('api/', include('product.urls')),
     # path('api/search', include('search.urls')),
     path('silk/', include('silk.urls', namespace='silk')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
